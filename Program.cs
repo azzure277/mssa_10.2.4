8b240ac7628d6296b35262676ce3024c909c3be1
azzure277 <brandon.govender21@outlook.com>
2025-09-10 15:11:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mssa_10._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+            Console.WriteLine("The members of the list are :");
+            foreach (var n in numbers)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine("\nThe numbers greater than 80 are :");
+            var result = numbers.Where(x => x > 80);
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
